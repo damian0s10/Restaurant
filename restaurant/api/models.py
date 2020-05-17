7c95a96b85a136eb7b3b7a1540c5300711f03500
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2020-05-17 17:14:18 +0200

    Add a plural name in the category model

--- restaurant/api/models.py
+++ restaurant/api/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
