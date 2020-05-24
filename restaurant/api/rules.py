fe59f418119139ad2a7d514d1e9c1fe610a0a035
damian0s10 <damian.wasiluk10@gmail.com>
2020-05-24 17:02:15 +0200

    new discounts

--- restaurant/api/rules.py
+++ restaurant/api/rules.py
@@ -34,6 +34,9 @@ class ProductDiscount(KnowledgeEngine):
     DISCOUNT_ALKOHOL_20_PERCENT = 'znizka.alkohol_20_procent_taniej'
     DISCOUNT_FREE_SAUCE = 'znizka.sos_gratis'
     DISCOUNT_DESSERT_10_PERCENT = 'znizka.deser_10_procent_taniej'
+    DISCOUNT_BEER_ONE_FREE = 'znizka.dwa_piwa_trzecie_gratis'
+    DISCOUNT_BURGER_AND_DRING_15_PERCENT = 'znizka.burger_z_napojem_15_procent_taniej'
+
     PACKAGE_SMALL_PIZZA = 'opakowanie.mala_pizza'
     PACKAGE_BIG_PIZZA = 'opakowanie.duza_pizza'
 
@@ -47,17 +50,17 @@ class ProductDiscount(KnowledgeEngine):
 
         for product in self.product_list:
             yield Product(name=product['name'], price=product['price'], category=product['category'],
-                          size=product['size'])
+                          size=product['size'], count=product['count'])
 
     @Rule()
     def default_values(self):
         self.declare(TotalPrice(total_price=0.0))
 
-    @Rule(AS.product << Product(name=MATCH.name, price=MATCH.price, category=MATCH.category, size=MATCH.size),
+    @Rule(AS.product << Product(name=MATCH.name, price=MATCH.price, category=MATCH.category, size=MATCH.size, count=MATCH.count),
           AS.sum_price << TotalPrice(total_price=MATCH.total_price))
-    def add_to_recipe(self, product, name, price, category, size, sum_price, total_price):
+    def add_to_recipe(self, product, name, price, category, size, count, sum_price, total_price):
         self.retract(product)
-        self.declare(Recipe(name=name, price=price, category=category, size=size))
+        self.declare(Recipe(name=name, price=price, category=category, size=size ,count=count))
         self.modify(sum_price, total_price=total_price + price)
 
     @Rule(NOT(Recipe(category=DISCOUNT_TWO_PIZZAS_ONE_PRICE)),
@@ -131,8 +134,7 @@ class ProductDiscount(KnowledgeEngine):
                              category=ProductDiscount.DISCOUNT_BURGER_15_PERCET))
 
     @Rule(Recipe(price=MATCH.price, category='sniadanie'),
-          TEST(lambda _: datetime.datetime.today().weekday() == 2),
-          TEST(lambda _: datetime.datetime.today().hour < 13))
+          TEST(lambda _: datetime.datetime.today().weekday() == 2 and datetime.datetime.today().hour < 13))
     def free_coffea(self):
         """
         Środa darmowa kawa przy zamówieniu oferty śniadaniowej, do godziny 13
@@ -142,8 +144,7 @@ class ProductDiscount(KnowledgeEngine):
                              category=ProductDiscount.DISCOUNT_FREE_COFFEA))
 
     @Rule(Recipe(price=MATCH.price, category='alkohol'),
-          TEST(lambda _: datetime.datetime.today().weekday() == 3),
-          TEST(lambda _: datetime.datetime.today().hour >= 20))
+          TEST(lambda _: datetime.datetime.today().weekday() == 3 and datetime.datetime.today().hour >= 20))
     def discount_alkohol(self, price):
         """
         Czwartek od godz. 20 alkohol - 20%
@@ -157,20 +158,22 @@ class ProductDiscount(KnowledgeEngine):
     def add_package(self, size):
         """
         Dodanie opakowanie do każdej pizzy
+        :param size:
+        :return:
         """
         if size == 1:
             self.declare(Product(name='Małe opakowanie do pizza', price=1.0,
-                             category=ProductDiscount.PACKAGE_SMALL_PIZZA))
+                                category=ProductDiscount.PACKAGE_SMALL_PIZZA))
         else:
             self.declare(Product(name='Duże opakowanie do pizza', price=1.5,
-                             category=ProductDiscount.PACKAGE_BIG_PIZZA))
+                                category=ProductDiscount.PACKAGE_BIG_PIZZA))
 
     @Rule(Recipe(category='pizza'),
-          TEST(lambda _: datetime.datetime.today().weekday() > 0),
-          TEST(lambda _: datetime.datetime.today().weekday() <= 3))
+          TEST(lambda _: 0 < datetime.datetime.today().weekday() <= 3))
     def free_sauce(self):
         """
         Sos gratis do każdej pizzy (od poniedziałku do czwartku)
+        :return:
         """
         self.declare(Product(name='Sos gratis', price=0.0,
                              category=ProductDiscount.DISCOUNT_FREE_SAUCE))
@@ -180,36 +183,67 @@ class ProductDiscount(KnowledgeEngine):
     def discount_desserts(self, price):
         """
         Niedziela znizka na desery 10%
+        :param price:
+        :return:
         """
         self.declare(Product(name='Niedziela zniżka na desery 10%', price=-(price * 0.1),
                              category=ProductDiscount.DISCOUNT_DESSERT_10_PERCENT))
 
     
-                             
+    @Rule(NOT(Recipe(category=DISCOUNT_BEER_ONE_FREE)),
+          Recipe(name=MATCH.name, category='alkohol', count=MATCH.count),
+          TEST(lambda _: datetime.datetime.today().weekday() == 4 and 16 <= datetime.datetime.today().hour < 20))
+    def free_beer(self,name,count):
+        """
+        Piątek w godz 16-20, 3 takie same piwa w cenie 2
+        :param name:
+        :param count:
+        :return:
+        """
+        for _ in range(count//2):
+            self.declare(Product(name='Piątek w godz. 16-20 darmowe piwo ' + name, price=0.0,
+                                 category=ProductDiscount.DISCOUNT_BEER_ONE_FREE))
+
+
+    @Rule(Recipe(category='burgery', price=MATCH.price1),
+          Recipe(category='napoje', price=MATCH.price2),
+          TEST(lambda _: datetime.datetime.today().weekday() == 6))
+    def discount_set_burger_drink(self,price1,price2):
+        """
+        Niedziela w bruger i napój w zestawie taniej o 15%
+        :param price1
+        :param price2
+        :return:
+        """
+        self.declare(Product(name='Niedziela zniżka na burger i napój w zestawie', price=round(-((price1+price2)*0.15),2),
+                                 category=ProductDiscount.DISCOUNT_BURGER_AND_DRING_15_PERCENT))
+
 # TODO 1. Kody rabatowe ( np. x% znizki na zamowienie, darmowa dostawa, sprawdzenie czy łącza się z innymi promocjami)
-# TODO 5. Piątek w godz 16-20, 3 piwa w cenie 2.
-# TODO 7. Niedziela w zestawie dowolne danie i napojem, -15% na zestaw
+
 
 
 
 if __name__ == '__main__':
     # TODO dummy data, can be removed later
     products = [
-        {'name': 'duza pizza', 'price': 12.5, 'category': 'pizza', 'size': 2},
-        {'name': 'duza pizza2', 'price': 32.5, 'category': 'pizza', 'size': 2},
-        {'name': 'duza pizza3', 'price': 12.5, 'category': 'pizza', 'size': 1},
-        {'name': 'duza pizza4', 'price': 12.5, 'category': 'pizza', 'size': 1},
-        {'name': 'duza pizza5', 'price': 12.5, 'category': 'pizza', 'size': 2},
-        {'name': 'duza pizza6', 'price': 12.5, 'category': 'pizza', 'size': 2},
-        {'name': 'Salatka 1', 'price': 15.0, 'category': 'salatka', 'size': 2},
-        {'name': 'Salatka 1', 'price': 15.0, 'category': 'salatka', 'size': 2},
-        {'name': 'Burger 1', 'price': 19.0, 'category': 'burgery', 'size': 2},
-        {'name': 'Jajecznica', 'price': 12.0, 'category': 'sniadanie', 'size': 2},
-        {'name': 'Nalesniki', 'price': 13.0, 'category': 'sniadanie', 'size': 2},
-        {'name': 'Piwo', 'price': 6.50, 'category': 'alkohol', 'size': 2},
-        {'name': 'Piwo2', 'price': 6.50, 'category': 'alkohol', 'size': 2},
-        {'name': 'Piwo3', 'price': 6.50, 'category': 'alkohol', 'size': 2},
-        {'name': 'Lody', 'price': 9.00, 'category': 'desery', 'size': 2}
+        {'name': 'duza pizza', 'price': 12.5, 'category': 'pizza', 'size': 2, 'count': 1},
+        {'name': 'duza pizza2', 'price': 32.5, 'category': 'pizza', 'size': 2, 'count': 1},
+        {'name': 'duza pizza3', 'price': 12.5, 'category': 'pizza', 'size': 1, 'count': 1},
+        {'name': 'duza pizza4', 'price': 12.5, 'category': 'pizza', 'size': 1, 'count': 1},
+        {'name': 'duza pizza5', 'price': 12.5, 'category': 'pizza', 'size': 2, 'count': 1},
+        {'name': 'duza pizza6', 'price': 12.5, 'category': 'pizza', 'size': 2, 'count': 1},
+        {'name': 'Salatka 1', 'price': 15.0, 'category': 'salatka', 'size': 2, 'count': 1},
+        {'name': 'Salatka 1', 'price': 15.0, 'category': 'salatka', 'size': 2, 'count': 1},
+        {'name': 'Burger 1', 'price': 19.0, 'category': 'burgery', 'size': 2, 'count': 1},
+        {'name': 'Jajecznica', 'price': 12.0, 'category': 'sniadanie', 'size': 2, 'count': 1},
+        {'name': 'Nalesniki', 'price': 13.0, 'category': 'sniadanie', 'size': 2, 'count': 1},
+        {'name': 'Piwo', 'price': 6.50, 'category': 'alkohol', 'size': 2, 'count': 1},
+        {'name': 'Piwo2', 'price': 6.50, 'category': 'alkohol', 'size': 2, 'count': 1},
+        {'name': 'Piwo3', 'price': 6.50, 'category': 'alkohol', 'size': 2, 'count': 1},
+        {'name': 'Lody', 'price': 9.00, 'category': 'desery', 'size': 2, 'count': 1},
+        {'name': 'Piwo4', 'price': 6.70, 'category': 'alkohol', 'size': 2, 'count': 5},
+        {'name': 'Sok', 'price': 4.00, 'category': 'napoje', 'size': 2, 'count': 1},
+        
 
     ]
 
